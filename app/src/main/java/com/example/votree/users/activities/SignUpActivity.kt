13a149c1f04d10a5edbdd85a8002fa29a6e34a1d
thang0306 <tdoannam980@gmail.com>
2024-04-03 13:25:10 +0700

    Update SignUpActivity

--- app/src/main/java/com/example/votree/users/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/votree/users/activities/SignUpActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.votree.databinding.ActivitySignUpBinding
+import com.example.votree.users.models.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +30,7 @@ class SignUpActivity : AppCompatActivity() {
         binding.button.setOnClickListener {
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
+            val username = binding.usernameEt.text.toString()
             val confirmPass = binding.confirmPassEt.text.toString()
 
             if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
@@ -34,11 +38,16 @@ class SignUpActivity : AppCompatActivity() {
 
                     firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
                         if (it.isSuccessful) {
-                            val intent = Intent(this, SignInActivity::class.java)
-                            startActivity(intent)
+                            val user = User(
+                                id = firebaseAuth.currentUser!!.uid, // Use the UID
+                                email = email,
+                                username = username,
+                                password = pass
+                            )
+
+                            storeUserInFirestore(user)
                         } else {
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-
                         }
                     }
                 } else {
@@ -46,8 +55,19 @@ class SignUpActivity : AppCompatActivity() {
                 }
             } else {
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
-
             }
         }
+    }
+
+    private fun storeUserInFirestore(user: User) {
+        db.collection("users").document(user.id)
+            .set(user)
+            .addOnSuccessListener {
+                val intent = Intent(this, SignInActivity::class.java)
+                startActivity(intent)
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(this, "Failed to create user: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
