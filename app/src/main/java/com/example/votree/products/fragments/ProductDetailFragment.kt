14a79c51aafd3de78278bf699926bf4b8eaa4be7
Thang0306 <dnthang21@clc.fitus.edu.vn>
2024-04-12 00:34:18 +0700

    - Implememt product review and rating. - View this review in the product detail

--- app/src/main/java/com/example/votree/products/fragments/ProductDetailFragment.kt
+++ app/src/main/java/com/example/votree/products/fragments/ProductDetailFragment.kt
@@ -12,91 +12,119 @@ import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.votree.R
 import com.example.votree.databinding.FragmentProductDetailBinding
+import com.example.votree.products.adapters.UserReviewAdapter
+import com.example.votree.products.models.ProductReview
 import com.example.votree.products.repositories.ProductRepository
 import com.example.votree.products.view_models.CartViewModel
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 class ProductDetailFragment : Fragment() {
 
-    private val args: ProductDetailFragmentArgs by navArgs()
     private lateinit var binding: FragmentProductDetailBinding
+    private val args: ProductDetailFragmentArgs by navArgs()
     private val cartViewModel = CartViewModel()
     private val firestore = FirebaseFirestore.getInstance()
     private val productRepository = ProductRepository(firestore)
+    private lateinit var userReviewAdapter: UserReviewAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentProductDetailBinding.inflate(inflater, container, false)
-
-        setUpProductView(args)
-
-        // Determine the user role and adjust UI elements
-        val isSeller = args.role == "store"
-        updateUIForUserRole(isSeller)
-
+        setupUI()
         return binding.root
     }
 
-    private fun updateUIForUserRole(isSeller: Boolean) {
-        if (isSeller) {
-            binding.chattingBtn.visibility = View.GONE
-            binding.addToCartBtn.visibility = View.GONE
-
-            binding.updateBtn.visibility = View.VISIBLE
-            binding.updateBtn.setOnClickListener {
-                val action =
-                    ProductDetailFragmentDirections.actionProductDetail2ToUpdateProduct(args.currentProduct)
-                findNavController().navigate(action)
-            }
-
-            binding.deleteBtn.visibility = View.VISIBLE
-            binding.deleteBtn.setOnClickListener {
-                AlertDialog.Builder(requireContext())
-                    .setTitle("Delete Product")
-                    .setMessage("Are you sure you want to delete this product?")
-                    .setPositiveButton("Accept") { dialog, which ->
-                        productRepository.deleteProduct(args.currentProduct) {
-                            if (it) {
-                                findNavController().popBackStack()
-                            } else {
-                                Log.e("ProductDetail", "Error deleting product")
-                            }
-                        }
-                    }
-                    .setNegativeButton("Cancel", null)
-                    .show()
-            }
-        } else {
-            binding.addToCartBtn.setOnClickListener {
-                cartViewModel.addProductToCart(args.currentProduct.id, 1)
-            }
-        }
-        binding.buyNowBtn.visibility = if (!isSeller) View.VISIBLE else View.GONE
+    private fun setupUI() {
+        displayProductDetails()
+        setupButtons()
+        setupReviewAdapter()
+        fetchAndDisplayReviews()
     }
 
-    private fun setUpProductView(args: ProductDetailFragmentArgs) {
-        args.currentProduct.let { product ->
-            binding.productPrice.text = context?.getString(R.string.price_format, product.price)
-            binding.description.text = product.description
-            binding.productType.text = product.type.toString()
-            binding.suitEnvironment.text = product.suitEnvironment.toString()
-            binding.suitClimate.text = product.suitClimate.toString()
-            binding.productRating.text = product.averageRate.toString()
-            binding.productSoldQuantity.text = product.quantitySold.toString()
-        }
+    private fun displayProductDetails() {
+        with(binding) {
+            args.currentProduct.let { product ->
+                productPrice.text = getString(R.string.price_format, product.price)
+                description.text = product.description
+                productType.text = product.type.toString()
+                suitEnvironment.text = product.suitEnvironment.toString()
+                suitClimate.text = product.suitClimate.toString()
+                productRating.text = product.averageRate.toString()
+                productSoldQuantity.text = product.quantitySold.toString()
 
-        try {
-            Glide.with(this)
-                .load(args.currentProduct.imageUrl)
-                .centerCrop()
-                .placeholder(R.drawable.img_placeholder)
-                .into(binding.productImage)
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.d("ProductDetail", "Error loading image")
+                Glide.with(this@ProductDetailFragment)
+                    .load(product.imageUrl)
+                    .centerCrop()
+                    .placeholder(R.drawable.img_placeholder)
+                    .into(productImage)
+            }
         }
+    }
+
+    private fun setupButtons() {
+        with(binding) {
+            val isSeller = args.role == "store"
+            if (isSeller) {
+                addToCartBtn.visibility = View.GONE
+                buyNowBtn.visibility = View.GONE
+                updateBtn.visibility = View.VISIBLE
+                deleteBtn.visibility = View.VISIBLE
+
+                updateBtn.setOnClickListener { navigateToUpdateProduct() }
+                deleteBtn.setOnClickListener { confirmProductDeletion() }
+            } else {
+                addToCartBtn.visibility = View.VISIBLE
+                buyNowBtn.visibility = View.VISIBLE
+                addToCartBtn.setOnClickListener {
+                    cartViewModel.addProductToCart(args.currentProduct.id, 1)
+                }
+            }
+        }
+    }
+
+    private fun navigateToUpdateProduct() {
+        val action = ProductDetailFragmentDirections.actionProductDetail2ToUpdateProduct(args.currentProduct)
+        findNavController().navigate(action)
+    }
+
+    private fun confirmProductDeletion() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Delete Product")
+            .setMessage("Are you sure you want to delete this product?")
+            .setPositiveButton("Accept") { _, _ ->
+                productRepository.deleteProduct(args.currentProduct) { success ->
+                    if (success) findNavController().popBackStack()
+                    else Log.e("ProductDetail", "Error deleting product")
+                }
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    private fun setupReviewAdapter() {
+        userReviewAdapter = UserReviewAdapter(emptyList(), CoroutineScope(Dispatchers.Main))
+        binding.reviewRecyclerView.adapter = userReviewAdapter
+    }
+
+
+    private fun fetchAndDisplayReviews() {
+        val reviews = mutableListOf<ProductReview>()
+        // Go to the products/productId/reviews collection in Firestore
+        firestore.collection("products").document(args.currentProduct.id).collection("reviews")
+            .get()
+            .addOnSuccessListener { reviewsSnapshot ->
+                for (reviewDocument in reviewsSnapshot.documents) {
+                    val review = reviewDocument.toObject(ProductReview::class.java)
+                    review?.let { reviews.add(it) }
+                }
+                setupReviewAdapter()
+            }
+            .addOnFailureListener { e ->
+                Log.e("ProductDetail", "Error fetching reviews", e)
+            }
     }
 }
