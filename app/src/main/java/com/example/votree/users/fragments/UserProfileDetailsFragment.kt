50a72c0dde13fffc6881ba84a7e64726344ab33a
Thang0306 <dnthang21@clc.fitus.edu.vn>
2024-04-30 22:16:43 +0700

    - Implement User Profile view. - Implement User Navigation Button.

--- app/src/main/java/com/example/votree/users/fragments/UserProfileDetailsFragment.kt
+++ app/src/main/java/com/example/votree/users/fragments/UserProfileDetailsFragment.kt
@@ -5,14 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.votree.R
+import androidx.lifecycle.lifecycleScope
+import com.example.votree.databinding.FragmentUserProfileDetailsBinding
+import com.example.votree.products.repositories.PointTransactionRepository
+import com.example.votree.users.repositories.UserRepository
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.launch
 
 class UserProfileDetailsFragment : Fragment() {
+    private var _binding: FragmentUserProfileDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    private val userId = FirebaseAuth.getInstance().currentUser?.uid ?: ""
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile_details, container, false)
+    ): View {
+        _binding = FragmentUserProfileDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val userRepository = UserRepository(FirebaseFirestore.getInstance())
+        val pointTransactionRepository = PointTransactionRepository()
+
+        lifecycleScope.launch {
+            val user = userRepository.getUser(userId)
+            user?.let {
+                binding.userNameTv.text = it.username
+                binding.userEmailTv.text = it.email
+                binding.userPhoneNumberTv.text = it.phoneNumber
+                binding.userAddressTv.text = it.address
+                binding.createdDateTv.text = it.createdAt.toString()
+
+                // Fetch and update accumulated points within the same scope
+                val userPoints = pointTransactionRepository.getCurrentPoints()
+                binding.accumulatePointTv.text = userPoints.toString()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/votree/users/fragments/UserProfileFragment.kt
+++ app/src/main/java/com/example/votree/users/fragments/UserProfileFragment.kt
@@ -1,46 +1,115 @@
-package com.example.votree.products.fragments
+package com.example.votree.users.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.example.votree.databinding.FragmentUserProfileDetailsBinding
+import com.example.votree.R
+import com.example.votree.databinding.FragmentUserProfileBinding
+import com.example.votree.users.activities.OrderHistoryActivity
+import com.example.votree.users.activities.RegisterToSeller
+import com.example.votree.users.activities.SignInActivity
 import com.example.votree.users.repositories.UserRepository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.launch
 
-class UserProfileDetailsFragment : Fragment() {
-    private var _binding: FragmentUserProfileDetailsBinding? = null
+class UserProfileFragment : Fragment() {
+    private var _binding: FragmentUserProfileBinding? = null
     private val binding get() = _binding!!
-
-    private val userId = FirebaseAuth.getInstance().currentUser?.uid ?: ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentUserProfileDetailsBinding.inflate(inflater, container, false)
+        _binding = FragmentUserProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val userRepository = UserRepository(FirebaseFirestore.getInstance())
+        setupView()
+        setupClickListeners()
+        loadUserProfileDetailsFragment()
+    }
 
+    private fun setupView(){
+        val userRepository = UserRepository(FirebaseFirestore.getInstance())
+        val userId = FirebaseAuth.getInstance().currentUser?.uid ?: ""
         lifecycleScope.launch {
             val user = userRepository.getUser(userId)
             user?.let {
-                binding.userNameTv.text = it.username
-                binding.userEmailTv.text = it.email
-                binding.userPhoneNumberTv.text = it.phoneNumber
-                binding.userAddressTv.text = it.address
-                binding.accumulatePointTv.text = it.accumulatePoint.toString()
-                binding.createdDateTv.text = it.createdAt.toString()
+                binding.userfullNameTv.text = user.fullName
+                binding.userRoleTv.text = user.role
+
+                if (user.role == "store") {
+                    disableBecomeSeller()
+                }
             }
         }
+    }
+
+    private fun disableBecomeSeller() {
+        // Disable the becomeSeller layout
+        binding.becomeSellerLayout.isEnabled = false
+        // Change the text color to gray
+        binding.becomeSellerTv.setTextColor(
+            ContextCompat.getColor(requireContext(), R.color.md_theme_outlineVariant)
+        )
+    }
+
+    private fun loadUserProfileDetailsFragment() {
+        val userProfileDetailsFragment = UserProfileDetailsFragment()
+        childFragmentManager.beginTransaction()
+            .replace(R.id.userProfileDetail_fcv, userProfileDetailsFragment)
+            .commit()
+    }
+
+    private fun setupClickListeners() {
+        binding.settingLayout.setOnClickListener {
+            // Navigate to Settings
+//            navigateToSettings()
+        }
+
+        binding.ordersLayout.setOnClickListener {
+            // Navigate to Orders
+            navigateToOrders()
+        }
+
+        binding.becomeSellerLayout.setOnClickListener {
+            // Navigate to Become Seller
+            navigateToBecomeSeller()
+        }
+
+        binding.logoutBtn.setOnClickListener {
+            // Navigate to Sign In Activity
+            navigateLogout()
+        }
+    }
+
+    private fun navigateToSettings() {
+        // Implement navigation logic to Settings
+        TODO()
+    }
+
+    private fun navigateToOrders() {
+        val intent = Intent(context, OrderHistoryActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToBecomeSeller() {
+        val intent = Intent(context, RegisterToSeller::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateLogout(){
+        SignInActivity().signOut()
+        val intent = Intent(this.context, SignInActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onDestroyView() {
